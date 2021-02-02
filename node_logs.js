7f2b47ad51a7d1db7753d2333cd42da54286989d
Josh Wilson <joshuef@gmail.com>
2021-02-02 13:20:26 +0100

    feat: filter logs on text input w/ debounce

--- node_logs.js
+++ node_logs.js
@@ -125,8 +125,28 @@ function parseLogFile(content) {
     
 }
 
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
 function bindDisplayFilters () {
     function reParseData () {
+        console.info("Reparse of data triggered.")
         nodeChartLines = [];
         allLogLines = [];
         window.chart=null;
@@ -139,6 +159,10 @@ function bindDisplayFilters () {
     };
 
     $("#apply-filter").click(reParseData);
+    $("#filter-text").on('input',debounce( function(e) {
+        console.log("debounced filter");
+        reParseData()
+    }, 250));
     $("#show-level").change(reParseData);
     $("#show-time").change(reParseData);
     $("#show-src-line").change(reParseData);
