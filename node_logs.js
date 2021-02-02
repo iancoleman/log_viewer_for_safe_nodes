2e858da65724184f3b3f3940e067d6a8de18d063
Josh Wilson <joshuef@gmail.com>
2021-02-02 14:58:56 +0100

    chore: add a bit of padding

--- node_logs.js
+++ node_logs.js
@@ -129,6 +129,7 @@ function parseLogFile(content) {
 // be triggered. The function will be called after it stops being called for
 // N milliseconds. If `immediate` is passed, trigger the function on the
 // leading edge, instead of the trailing.
+// this func sourced here: https://davidwalsh.name/javascript-debounce-function
 function debounce(func, wait, immediate) {
 	var timeout;
 	return function() {
